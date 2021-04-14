76db9bdfdb8d44d571c0e97b86240f04b2da95ca
davidmorrier <davidmorrier@gmail.com>
2021-04-14 10:44:32 -0400

    events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,27 @@
+import { Fragment } from 'react'
+import { useRouter } from 'next/router'
+
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
+
 function AllEventsPage() {
+	const router = useRouter()
+	const events = getAllEvents()
+
+	function findEventsHandler(year, month) {
+		const fullpath = `/events/${year}/${month}`;
+		
+		router.push(fullpath)
+	}
+
+
 	return (
-		<div>
-			<h1> All Events </h1>{" "}
-		</div>
+		<Fragment>
+			<EventsSearch onSearch={findEventsHandler} />
+			<EventList items={events} />
+			</Fragment>
+		
 	);
 }
 
